e4a56e06d4c8afac0ac06aeaad5a55f6d882ec84
Andrew <proger0605@mail.ru>
2015-06-29 17:25:15 +0800

    add

---
+++ Crypt/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Numerics;
+namespace Crypt
+{
+    public partial class Form1 : Form
+    {
+        private int Rand()//Ф-я получения случайного числа
+        {
+            Random random = new Random();
+            return random.Next();
+        }
+        int power(int a, int b, int n) // a^b mod n - возведение a в степень b по модулю n
+        {
+            int tmp = a;
+            int sum = tmp;
+            for (int i = 1; i < b; i++)
+            {
+                for (int j = 1; j < a; j++)
+                {
+                    sum += tmp;
+                    if (sum >= n)
+                    {
+                        sum -= n;
+                    }
+                }
+                tmp = sum;
+            }
+            return tmp;
+        }
+        int mul(int a, int b, int n) // a*b mod n - умножение a на b по модулю n
+        {
+            int sum = 0;
+            for (int i = 0; i < b; i++)
+            {
+                sum += a;
+                if (sum >= n)
+                {
+                    sum -= n;
+                }
+            }
+            return sum;
+        }
+        void crypt(int p, int g, int x, string strIn)
+        {
+            richTextBox2.Text = "";
+            int y = power(g, x, p);
+            if (strIn.Length > 0)
+            {
+                char[] temp = new char[strIn.Length - 1];
+                temp = strIn.ToCharArray();
+                for (int i = 0; i <= strIn.Length - 1; i++)
+                {
+                    int m = (int)temp[i];
+                    if (m > 0)
+                    {
+                        int k = Rand() % (p - 2) + 1; // 1 < k < (p-1)
+                        int a = power(g, k, p);
+                        int b = mul(power(y, k, p), m, p);
+                        richTextBox2.Text = richTextBox2.Text + a + " " + b + " ";
+                    }
+                }
+            }
+        }
+        void decrypt(int p, int x, string strIn)
+        {
+            if (strIn.Length > 0)
+            {
+                richTextBox3.Text = "";
+                string[] strA = strIn.Split(' ');
+                if (strA.Length > 0)
+                {
+                    for (int i = 0; i < strA.Length - 1; i += 2)
+                    {
+                        char[] a = new char[strA[i].Length];
+                        char[] b = new char[strA[i + 1].Length];
+                        int ai = 0;
+                        int bi = 0;
+                        a = strA[i].ToCharArray();
+                        b = strA[i + 1].ToCharArray();
+                        for (int j = 0; (j < a.Length); j++)
+                        {
+                            ai = ai * 10 + (int)(a[j] - 48);
+                        }
+                        for (int j = 0; (j < b.Length); j++)
+                        {
+                            bi = bi * 10 + (int)(b[j] - 48);
+                        }
+                        if ((ai != 0) && (bi != 0))
+                        {
+                            int deM = mul(bi, power(ai, p - 1 - x, p), p);// m=b*(a^x)^(-1)mod p =b*a^(p-1-x)mod p - трудно было  найти нормальную формулу, в ней вся загвоздка
+                            char m = (char)deM;
+                            richTextBox3.Text = richTextBox3.Text + m;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strIn = richTextBox1.Text;
+            int p = Convert.ToInt32(textBox1.Text);
+            int g = Convert.ToInt32(textBox2.Text);
+            int x = Convert.ToInt32(textBox3.Text);
+            crypt(p, g, x, strIn);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strOut = richTextBox2.Text;
+            int p = Convert.ToInt32(textBox1.Text);
+            int x = Convert.ToInt32(textBox3.Text);
+            decrypt(p, x, strOut);
+        }
+    }
+}
